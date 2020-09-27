91674cd83d13c943ab7a13db1bae64802f556777
ivan-piminov <ivan-piminov@yandex.ru>
2020-09-27 12:10:52 +0300

    Updates

---
+++ precache-manifest.72f68f1fe645800d53d7b7d804f9a49f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7c790d9ad3086d4d38706ddfdc9047",
+    "url": "/TestTable/index.html"
+  },
+  {
+    "revision": "1ca25b6becb75b25b33e",
+    "url": "/TestTable/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "35cb0bea2ab47e4ec2d0",
+    "url": "/TestTable/static/css/main.18dd0dd9.chunk.css"
+  },
+  {
+    "revision": "1ca25b6becb75b25b33e",
+    "url": "/TestTable/static/js/2.7a26c3e7.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/TestTable/static/js/2.7a26c3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cb0bea2ab47e4ec2d0",
+    "url": "/TestTable/static/js/main.1c9f8854.chunk.js"
+  },
+  {
+    "revision": "5e785315cbec14d854ed",
+    "url": "/TestTable/static/js/runtime-main.bf729da2.js"
+  }
+]);
